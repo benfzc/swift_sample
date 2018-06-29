a9ffb26db10afc987f2884c51aecf91fd531044e
benfzc <benfzcfr.ee@gmail.com>
2018-06-29 21:32:34 +0800

    [New] Add a sample to show how to use default camera to take a picture

---
+++ CameraPicker/CameraPicker/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  CameraPicker
+//
+//  Created by ben on 2018/6/29.
+//  Copyright © 2018年 ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        showCamera()
+    }
+    
+
+}
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+ 
+    func showCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated:true, completion: nil)
+    }
+    
+}
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ## ThreeLevelNavigation
 - a sample that demonstrate how to use navigation controller to navigate 
   3-level view controllers and how to pass data between them
+
+## CameraPicker
+- a sample that demonstrate how to detect image touch and how to use built-in camera to take a picture
